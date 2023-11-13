b8beb4e592d852bae6790141fd509f0ea0400dbe
Michael Ndula <michaelndula@gmail.com>
2023-11-13 20:26:55 +0300

    adding the bottom navigation tabs

--- navigation/screens/Notes.js
+++ navigation/screens/Notes.js
@@ -1,12 +1,15 @@
-import * as React from 'react';
-import { View, Text} from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
 
-export default function Notes({navigation}){
-    return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-            <Text 
-            onPress={() => navigation.navigate('Home')}
-            style={{fontSize: 26, fontWeight: 'bold'}}>Notes</Text>
-        </View>
-    )
+export default function Notes({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text
+        onPress={() => navigation.navigate("Home")}
+        style={{ fontSize: 26, fontWeight: "bold" }}
+      >
+        Notes
+      </Text>
+    </View>
+  );
 }
--- navigation/screens/SermonsScreen.js
+++ navigation/screens/SermonsScreen.js
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { View, Text } from "react-native";
 
-export default function More({ navigation }) {
+export default function Sermons({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text
         onPress={() => navigation.navigate("Home")}
         style={{ fontSize: 26, fontWeight: "bold" }}
       >
-        More
+        Sermons
       </Text>
     </View>
   );
