0624891ff651c3e0c930f22e13817de3de1f2287
mathew <69637595+wa2f@users.noreply.github.com>
2023-11-25 18:28:14 +0300

    navigation screens

--- App.js
+++ App.js
@@ -6,6 +6,9 @@ import LandingScreen from './navigation/LandingScreen';
 import LoginScreen from './navigation/screens/auth/LoginScreen';
 import SignupScreen from './navigation/screens/auth/SignupScreen';
 
+import ProfileScreen from './navigation/screens/auth/ProfileScreen';
+import SettingScreen from './navigation/screens/auth/SettingScreen';
+
 const Stack = createStackNavigator();
 
 function App() {
@@ -15,8 +18,14 @@ function App() {
         <Stack.Screen name="LandingScreen" component={LandingScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignupScreen" component={SignupScreen} />
+        <Stack.Screen name="MainContainer" component={MainContainer} />
+
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+
+    // <MainContainer />
   )
 }
 
