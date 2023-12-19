f34aaab798f070283df616bca596f4cb8e36184f
mathewwahome <methewwahome@gmail.com>
2023-12-19 09:48:47 +0300

    completion of the auth

---
+++ assets/css/styles.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  Image,
+  TextInput,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  
+  inputContainer: {
+    flexDirection: "col",
+    alignItems: "start",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 20,
+  },
+
+  input: {
+    flex: 1,
+    marginLeft: 10,
+    height:50,
+
+  },
+  textarea: {
+    flex: 1,
+    marginLeft: 10,
+    height:100,
+
+  },
+  
+  
+});
+export { styles };
