52d93f3644a20523d97d17f96d8002dc45ae9109
Michaelndula <michaelndula@gmail.com>
2024-01-17 10:21:50 +0300

    Handle Signup and Login separately

--- src/hooks/HandleApis.js
+++ src/hooks/HandleApis.js
@@ -8,48 +8,6 @@ const config = {
 };
 
 export const BASE_URL = 'http://karencommunitychurch.org:8089';
-export const FILE_BASE = BASE_URL;
-
-export const handleLogin = async (email, password, setUserId, navigation) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/api/login`, {
-      email,
-      password,
-    });
-    const token = response.data.token;
-    const loggedId = response.data.userId;
-    setUserId(loggedId);
-    navigation.navigate('MainContainer');
-  } catch (error) {
-    console.error('Login failed:', error);
-    throw error;
-  }
-};
-
-export const handleRegister = async (
-  name,
-  email,
-  phone,
-  password,
-  navigation,
-) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/api/register`, {
-      name,
-      email,
-      phone,
-      password,
-    });
-    if (response && response.data) {
-      const token = response.data.token;
-      navigation.navigate('MainContainer');
-    } else {
-      console.error('Registration failed: No data in the response');
-    }
-  } catch (error) {
-    console.error('Registration failed:', error.message || error);
-  }
-};
 
 const URL = `${BASE_URL}/api/`;
 
