9587c9fc2ac2c152f0086e8f0ea755d5110c0a76
mathewwahome <mathewwahome2003@gmail.com>
2024-01-19 10:58:03 +0300

    sermon basics

--- src/screens/SermonsScreen.js
+++ src/screens/SermonsScreen.js
@@ -1,70 +1,75 @@
 import React, {useEffect, useState} from 'react';
-import {
-  View,
-  ScrollView,
-  Text,
-  Image,
-  ImageBackground,
-  StyleSheet,
-} from 'react-native';
+import {View, ScrollView, Text, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
-import {styles} from '../assets/css/SermonsScreen';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {BASE_URL, fetchDataByEndpoint} from '../hooks/HandleApis';
-
+import {styles} from '../assets/css/Global';
+export const fetchSermons = async () => {
+  return fetchDataByEndpoint('fetchSermons');
+};
 export default function Sermons({userId}) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
-
-  const url = `${BASE_URL}/api/fetchSermons`;
+  const [sermonsData, setSermonsData] = useState([]);
+  const [sermonsLoading, setSermonsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(url)
-      .then(response => response.json())
-      .then(json => {
-        setData(json);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error(`Error fetching data from ${url}:`, error);
-        setLoading(false);
-      });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const sermons = await fetchSermons();
+        setSermonsData(sermons);
+        setSermonsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  // Function to redirect to sermon notes
-  const handlePress = (sermonId, userId) => {
-    console.log('Navigating with sermonId:', sermonId);
-    console.log('Navigating with userId:', userId);
-    navigation.navigate('SermonNotes', {sermonId, userId});
-  };
+    fetchData();
+  }, []);
 
   return (
     <ScrollView>
       <View style={{padding: 10}}>
         <ScrollView>
-          <View>
-            {loading ? (
-              <Text>Loading Sermons...</Text>
-            ) : (
-              data.map(sermon => (
-                <View style={styles.itemContainer} key={sermon.id}>
-                  <TouchableOpacity onPress={() => handlePress(sermon.id)}>
+          {sermonsLoading ? (
+            <Text>Loading sermons...</Text>
+          ) : sermonsData && sermonsData.length > 0 ? (
+            sermonsData.map(sermon => (
+              <TouchableOpacity
+                key={sermon.id}
+                onPress={() =>
+                  navigation.navigate('VideoPlayer', {sermon: sermon})
+                }>
+                <View style={{flexDirection: 'column', padding: 10}}>
+                  <View style={{marginRight: 10}}>
                     <Image
                       style={styles.image}
                       source={{
                         uri: `${BASE_URL}/SermonThumbnails/${sermon.Thumbnail}`,
                       }}
                     />
-                    <Text style={styles.text}>{sermon.Title}</Text>
                     <Text style={styles.text}>
-                      {sermon.Sermon_Description.slice(0, 20)}...
+                      {new Date(sermon.created_at).toLocaleDateString(
+                        undefined,
+                        {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                        },
+                      )}
                     </Text>
-                  </TouchableOpacity>
+                    <Text style={styles.title}>
+                      {sermon.Title.slice(0, 40)}...
+                    </Text>
+                    <Text style={styles.text}>
+                      {sermon.Sermon_Description.slice(0, 80)}...
+                    </Text>
+                  </View>
                 </View>
-              ))
-            )}
-          </View>
+              </TouchableOpacity>
+            ))
+          ) : (
+            <Text>No Sermons available</Text>
+          )}
         </ScrollView>
       </View>
     </ScrollView>
